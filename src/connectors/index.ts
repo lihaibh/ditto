521794637c026a40ef36f522f3346297111cbd2a
Lihai Ben-Haim <lihaibh@gmail.com>
2020-09-03 15:32:24 +0300

    refactor: prepare release for v1.0.0

---
+++ src/connectors/index.ts
@@ -0,0 +1,4 @@
+export * from './Connector';
+export * from './MongoDBDuplexConnector';
+export * from './filesystem/FileSystemDuplexConnector';
+export * from './filesystem/LocalFileSystemDuplexConnector';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./MongoTransferer";
 export * from "./contracts";
 export * from "./errors";
+export * from './connectors';
